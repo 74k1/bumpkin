52fd0aa80c792a96c675404a4a3972e78a08397f
74k1 <git.t@betsumei.com>
2026-05-13 13:57:09 +0200

    chore: project scaffolding (flake, Cargo, CI, license)

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "bumpkin: Rust upkeep bot for Nix flake package updates";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "bumpkin";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/bumpkin";
+        };
+      });
+
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.rustfmt
+              pkgs.clippy
+              pkgs.nix
+              pkgs.git
+              pkgs.curl
+            ];
+          };
+        });
+    };
+}
